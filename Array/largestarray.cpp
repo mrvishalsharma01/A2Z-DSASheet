d4f04364403327efe27144d1b9a8ecceba6e35e4
vishalsharma <outreach.vishal@gmail.com>
2024-12-23 09:21:23 +0530

    added largest array element problem

--- Array/largestarray.cpp
+++ Array/largestarray.cpp
@@ -1,6 +1,30 @@
+//Brute Force Approach
+//Solution1: Sorting O(nlogn)
+//Solution2: Using max variable TC: O(n) SC:O(1)
+
+
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout<<"Hello World\n";
+int largest(int arr[], int n){
+    int maxi=arr[0];
+    for(int i=0; i<n; i++){
+        if(arr[i]>maxi)
+        maxi=arr[i];
+    }
+    return maxi;
+
 }
+
+int main(){
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    cout<<largest(arr,n);
+}
+
