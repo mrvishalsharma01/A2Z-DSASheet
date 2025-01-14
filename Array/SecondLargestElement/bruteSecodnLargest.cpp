92899b71eff59e216af9d4553b5b1cfa50362c9a
vishalsharma <outreach.vishal@gmail.com>
2025-01-14 18:53:05 +0530

    added intution approach, time and space complexity

---
+++ Array/SecondLargestElement/bruteSecodnLargest.cpp
@@ -0,0 +1,12 @@
+//Solution Sorting 
+// Intutiion:- First of all we need to sort the array and check for the last second element which will always be the second largest element but
+// the array should not contain any duplicate element 
+
+// Approach:-
+// Sort the array element in ascending order
+// Check that there is no dupicate element in an array
+// Now the n-2 element will always be the second largest element 
+
+// Time Complexity:- O(n*log n)
+// Space Complexity:- O(n)
+
