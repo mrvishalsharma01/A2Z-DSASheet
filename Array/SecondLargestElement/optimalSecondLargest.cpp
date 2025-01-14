f41b57a6870cf12f026199c3ec8ed5c65ac82b4c
vishalsharma <outreach.vishal@gmail.com>
2025-01-14 20:04:29 +0530

    added optimal second largest

---
+++ Array/SecondLargestElement/optimalSecondLargest.cpp
@@ -0,0 +1,15 @@
+//  Optimal Approach
+// In the better we are doing 2 pass traversal but here we need to do in single traversal 
+// Now here if the current element is larger than update the second largest and large variable 
+// Else if the current elemt is largest than second largest than we will updte the variable second large 
+
+// Time Complexity:- o(n)
+// space complexity:-  o(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int secondLargestEle(int arr[], int n){
+    
+}
+
