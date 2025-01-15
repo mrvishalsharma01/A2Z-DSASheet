e93b1265433352c3ded0cae2a452c77ac3dc7a77
vishalsharma <outreach.vishal@gmail.com>
2025-01-15 09:56:14 +0530

    added brute force left rotate array problem soln

---
+++ Array/LeftRoatateArrayByOne/BruteLeftRotate.cpp
@@ -0,0 +1,33 @@
+// Brute Force Approach
+// The first approach that comes to my mind when i see this type of problem is 
+// we can make a new array and assing the value form previous array to that with 
+// till n-1 and we will store that in temp variable in last
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void leftRoatateArray(int arr[], int n){
+    int temp[n];
+    for(int i=1; i<n; i++){
+        temp[i-1]=arr[i];
+    }
+    temp[n-1]=arr[0];
+    for(int i=0; i<n; i++){
+        cout<<temp[i]<<" ";
+    }
+}
+
+int main(){
+    int n;
+    cin>>n; 
+
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    leftRoatateArray(arr,n);
+
+}
+
+
+
