60a2424b8af1811bb96128b3513ebe4a37468fe9
vishalsharma <outreach.vishal@gmail.com>
2025-01-15 10:04:13 +0530

    optimalleft rotate code added

---
+++ Array/LeftRoatateArrayByOne/OptimialLeftRotate.cpp
@@ -0,0 +1,28 @@
+//Optimal Approach
+//To optimise the code what we can do here is we and assing the first element in temp variable 
+// and print the value at time time of printing
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void LeftRotateArr(int arr[],int n){
+    int temp=arr[0];
+    for(int i=0; i<n; i++){
+        arr[i]=arr[i+1];
+    }
+    arr[n-1]=temp;
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+int main(){
+    int n;
+    cin>>n; 
+
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    LeftRotateArr(arr,n);
+
+}
