89f12dbe121d8140ccc6d02bbf8566c2ca3db53f
vishalsharma <outreach.vishal@gmail.com>
2025-01-15 10:08:56 +0530

    added time and space complexity

--- Array/RemoveDuplicates/BruteRemoveDuplicate.cpp
+++ Array/RemoveDuplicates/BruteRemoveDuplicate.cpp
@@ -5,6 +5,9 @@
 // put every element of the set in the array from the starting of array  
 // now return k
 
+//Time Complexity:- O(n*log n) + O(n)
+//Space Complexity:-  O(n)
+
 #include <bits/stdc++.h>
 using namespace std;
 
