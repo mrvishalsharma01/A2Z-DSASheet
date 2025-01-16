4da6d02cbb8862c70ce9cbb4184856f2e6733433
vishalsharma <outreach.vishal@gmail.com>
2025-01-16 09:13:17 +0530

    added brute force explanation in leftrotate array by d

---
+++ Array/LeftRotateArrayByD/BruteLeftrotate.cpp
@@ -0,0 +1,5 @@
+//Brute force Approach 
+
+// What we can do here is we can take a temp array and put it into the array 
+// Now here shift is happening i-d place i.e a[i-d]=ar[i]
+// Put back temp array from n-k
