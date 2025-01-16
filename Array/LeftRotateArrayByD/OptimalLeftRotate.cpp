9ff458f8b1ca619d1a0dca8d83f8171627c64a45
vishalsharma <outreach.vishal@gmail.com>
2025-01-16 09:31:43 +0530

    added optimal explanation and code

--- Array/LeftRotateArrayByD/OptimalLeftRotate.cpp
+++ Array/LeftRotateArrayByD/OptimalLeftRotate.cpp
@@ -3,3 +3,28 @@
 //reverse the array a+d to a+n
 //reverse the whole array a, a+n
 
+#include <bits/stdc++.h>
+using namespace std;
+
+void leftRotate(int arr[], int n, int d){
+    reverse(arr,arr+d);
+    reverse(arr+d, arr+n);
+    reverse (arr, arr+n);
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int d;
+    cin>>d;
+
+    leftRotate(arr,n,d);
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
