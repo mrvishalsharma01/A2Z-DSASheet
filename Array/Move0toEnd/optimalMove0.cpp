faab0a8205e0e700668949672fee00b36f6e2a35
vishalsharma <outreach.vishal@gmail.com>
2025-01-16 10:13:24 +0530

    added optimal solution and comments

--- Array/Move0toEnd/optimalMove0.cpp
+++ Array/Move0toEnd/optimalMove0.cpp
@@ -4,18 +4,18 @@
 using namespace std;
 
 vector<int>moveZeros(vector<int>&a, int n){
-    int j=-1;
+    int j=-1;// Index to track the position of the first zero in the array.
     for(int i=0; i<n; i++){
-        if(a[i]==0){
-            j=i;
-            break;
+        if(a[i]==0){// If a zero is found,
+            j=i;// store its index in 'j'.
+            break;// Exit the loop as we only need the first zero's index.
         }
     }
-    if(j==-1)return a;
+    if(j==-1)return a; // If no zeros are found, the array is already fine; return as is.
     for(int i=j+1; i<n; i++){
-        if(a[i]!=0){
-            swap(a[i],a[j]);
-            j++;
+        if(a[i]!=0){// If the current element is non-zero,
+            swap(a[i],a[j]);// Swap it with the zero at index 'j'.
+            j++;// Move 'j' to the next zero's position.
         }
     }
     return a;
