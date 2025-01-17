aed692fecdb38384c67415aa2be2b3378bfb4af5
vishalsharma <outreach.vishal@gmail.com>
2025-01-17 12:47:38 +0530

    added time and space complexity

--- Array/MaximumConsecutive/optimalMaxCons.cpp
+++ Array/MaximumConsecutive/optimalMaxCons.cpp
@@ -1,5 +1,8 @@
 //This is the optimal solution that we have to solve maximum consecutive ones 
 
+//Time Complexity:- O(n)
+//Space Complexity:- O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
