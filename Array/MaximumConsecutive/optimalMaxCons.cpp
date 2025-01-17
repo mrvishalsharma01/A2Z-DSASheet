a536decb24184df011ff70132f7575dd620e440e
vishalsharma <outreach.vishal@gmail.com>
2025-01-17 12:45:26 +0530

    added code and explained the process

---
+++ Array/MaximumConsecutive/optimalMaxCons.cpp
@@ -0,0 +1,30 @@
+//This is the optimal solution that we have to solve maximum consecutive ones 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMax(vector<int>&a, int n){
+    int count=0; 
+    int maxi=0;
+    for(int i=0; i<n; i++){
+        if(a[i]==1){
+        count++;
+        } else{
+            count=0;
+        }
+        maxi=max(maxi,count);
+    }
+    return maxi;
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    vector<int>arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    cout<<"The Maximum element is "<<findMax(arr,n);
+    return 0;
+}
